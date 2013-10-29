29815f438ac25949c0325a4b13f8ece9448d4f7c
Nathan Walton <nwalton@byu.edu>
2013-10-29 09:53:41 -0600

    Fix sliding menu

--- config.rb
+++ config.rb
@@ -9,7 +9,7 @@
 
 # Change this to :production when ready to deploy the CSS to the live server.
 #environment = :development
-environment = :production
+environment = :development
 
 # In development, we can turn on the FireSass-compatible debug_info.
 firesass = false
@@ -41,7 +41,7 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :development) ? :expanded : :compressed
+output_style = (environment == :development) ? :compact : :compressed
 
 # To enable relative paths to assets via compass helper functions. Since Drupal
 # themes can be installed in multiple locations, we don't need to worry about
@@ -49,8 +49,8 @@ output_style = (environment == :development) ? :expanded : :compressed
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 # Pass options to sass. For development, we turn on the FireSass-compatible
 # debug_info if the firesass config variable above is true.
-sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
+sass_options = (environment == :development && firesass == true) ? {:debug_info => false} : {}
